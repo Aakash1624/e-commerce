b4c3de42044f1a7ab1062a0ae9242be0a2590262
Aakash V <hariaakash.405@gmail.com>
2024-04-10 07:10:38 +0530

    commit

--- src/components/whatsapp/icon.jsx
+++ src/components/whatsapp/icon.jsx
@@ -1,11 +1,23 @@
+import './icon.css';
+
 const Whatsapp = () => {
   return (
     <div>
       <a href="https://wa.me/6383017428">
         <img
-          alt="Chat on WhatsApp"
-          src="/src/assets/images/whatsapp_icon.png"
+          className="whatImg"
+          src="https://static.vecteezy.com/system/resources/previews/018/930/746/non_2x/whatsapp-logo-whatsapp-icon-whatsapp-transparent-free-png.png"
+          alt=""
         />
+        whatsapp me
+      </a>
+      <a href="tel:6383017428">
+        <img
+          className="whatImg"
+          src="https://img.freepik.com/free-vector/phone_78370-560.jpg?size=338&ext=jpg&ga=GA1.1.1700460183.1712534400&semt=ais"
+          alt=""
+        />
+        Call me
       </a>
     </div>
   );
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,10 +1,9 @@
+import Whatsapp from '../components/whatsapp/icon';
+
 const Contact = () => {
   return (
     <div>
-      <a href="https://wa.me/6383017428">
-        <img src="/src/assets/images/whatsapp_icon.png" alt="" />
-        whatsapp me
-      </a>
+      <Whatsapp />
     </div>
   );
 };
